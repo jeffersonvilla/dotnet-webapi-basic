3433dad382d3cc58fc63ddf0dc3bff958ee154bf
Esteban Angulo <jangulorama@gmail.com>
2023-09-23 18:39:33 -0500

    first commit

---
+++ TODOApp/TODO.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TODO.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public UsersController() {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetHelloWorld() { 
+            return Ok("Hello World");
+        
+        }
+
+        
+    }
+}
